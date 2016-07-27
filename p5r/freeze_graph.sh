3e9b039f5cac70facb1781616767e8083ae1e365
Brad Stillwell <bspublicmail@gmail.com>
2016-07-27 19:04:01 +1200

    :snowflake: Added freezing model

---
+++ p5r/freeze_graph.sh
@@ -0,0 +1,5 @@
+~/github/tensorflow/bazel-bin/tensorflow/python/tools/freeze_graph \
+	--input_graph=save/model.pb \
+	--input_checkpoint=save/model.ckpt-0 \
+	--output_graph=save/frozen_graph.pb \
+	--output_node_names=predict_single_output
